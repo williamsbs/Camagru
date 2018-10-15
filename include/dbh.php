161b1a95916cc14bd8a29988d46c4e1b2d62dafc
William Sabates <williamsabates@MacBook-Pro-de-William.local>
2018-10-15 14:46:20 +0200

    d

--- include/dbh.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "root";
-$dbName = "loginsystem";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-
-?>
